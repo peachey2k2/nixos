b4c673176c93ebadcc3967d3db62856fea77ab10
peachey2k2 <ogzpolat@gmail.com>
2025-10-21 13:00:02 +0300

    21-10-2025

--- system.nix
+++ system.nix
@@ -72,8 +72,6 @@ in {
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
