0b5f342cda166a2bde6461d7b620cfaa2d134674
peachey2k2 <ogzpolat@gmail.com>
2025-11-04 18:00:01 +0300

    04-11-2025

--- system.nix
+++ system.nix
@@ -131,9 +131,27 @@ in {
     };
   };
 
-  nix.settings.experimental-features = [
-    "flakes" "nix-command"
-  ];
+  nix.settings = {
+    experimental-features = [
+      "flakes" "nix-command"
+    ];
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+      "https://cache.iog.io"
+      "https://cuda-maintainers.cachix.org"
+      "https://nixpkgs-unfree.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPiCe+467rJVel7/TrsBQQQTfvs5cBUOQ="
+      "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nqlt0="
+    ];
+    max-substitution-jobs = 32;
+    http-connections = 50;
+  };
 
 
   programs = {
